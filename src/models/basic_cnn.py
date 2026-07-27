6413e83db221689e62ca358880e65e26197926fd
ashutosh-012 <23bma012@nith.ac.in>
2026-07-28 02:55:17 +0530

    fix cnn padding shape

--- src/models/basic_cnn.py
+++ src/models/basic_cnn.py
@@ -6,17 +6,17 @@ class BasicCNN(nn.Module):
     def __init__(self, numClasses=8, inputChannels=2, seqLen=128):
         super(BasicCNN, self).__init__()
 
-        self.conv1 = nn.Conv1d(inputChannels, 64, kernel_size=8, padding=3)
+        self.conv1 = nn.Conv1d(inputChannels, 64, kernel_size=8, padding="same")
         self.bn1 = nn.BatchNorm1d(64)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool1d(2)
 
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv1d(64, 128, kernel_size=5, padding="same")
         self.bn2 = nn.BatchNorm1d(128)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool1d(2)
 
-        self.conv3 = nn.Conv1d(128, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv1d(128, 128, kernel_size=3, padding="same")
         self.bn3 = nn.BatchNorm1d(128)
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool1d(2)
